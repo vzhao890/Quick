030c330a31ad8123141f4eedf5f405e995e9faa5
BT_1N3_31 <@>
2018-11-27 10:14:05 -0500

    1

--- src/quickSort.java
+++ src/quickSort.java
@@ -1,31 +1,33 @@
 public class quickSort
 {
-    public void quickSort(int[] arr, int left, int right)
+    public static void quickSort(int[] arr, int left, int right)
     {
-        if(left>right)
+        if(left<right)
         {
             int pivot = partition(arr,left,right);
             quickSort(arr,left,pivot-1);
             quickSort(arr,pivot+1,right);
-
         }
 
     }
-    public int partition(int[] arr, int left,int right)
+    public static int partition(int[] arr, int left,int right)
     {
-        int pivot = right;
+        int pivot = arr[right];
         int i = left -1;
         for(int j = left; j<right; j++)
         {
             if(arr[j] <= pivot)
             {
-                i+=1;
-                int swap = arr[j];
+                i++;
+                int swap = arr[i];
                 arr[i] = arr[j];
                 arr[j] = swap;
             }
         }
-        arr[i+1] = arr[right];
+        int temp1=arr[right];
+        arr[right]=arr[i+1];
+        arr[i+1] = temp1;
         return i+1;
     }
+
 }
--- src/runner.java
+++ src/runner.java
@@ -1,4 +1,13 @@
 public class runner
 {
-
+    public static void main(String[] args)
+    {
+        int[] list1 = {1, 2, 3, 54, 4, 5, 5, 1};
+        quickSort.quickSort(list1,1,list1.length-1);
+        for(int i=0;i<list1.length;i++)
+        {
+            System.out.print(" "+list1[i]);
+        }
+        System.out.println();
+    }
 }
